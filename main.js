15049e3eed0e52a5725914b1a208cc6e192fe878
Muhammad Irsyad <irsyart@gmail.com>
2023-05-25 22:03:09 +0700

    feat: add vite

---
+++ main.js
@@ -0,0 +1,70 @@
+VANTA.TRUNK({
+  el: "#trunk",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xd7d7d7,
+  backgroundColor: 0x202020,
+  spacing: 8.00,
+  chaos: 7.00
+})
+
+gsap.registerPlugin(ScrambleTextPlugin);
+
+const scrambleRead = gsap.utils.toArray('.read');
+scrambleRead.forEach(item => {
+  let tween = gsap.to(item, {
+    duration: 1,
+    scrambleText:{
+      text:"読む",
+      chars:"ボゼ"
+    },
+    paused: true
+  });
+  item.addEventListener('mouseenter', () => {
+    tween.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    tween.reverse();
+  });
+});
+
+const scrambleGithub = gsap.utils.toArray('.github');
+scrambleGithub.forEach(item => {
+  let tween = gsap.to(item, {
+    duration: 1,
+    scrambleText:{
+      text:"ギットハブ",
+      chars:"ギトロユプ"
+    },
+    paused: true
+  });
+  item.addEventListener('mouseenter', () => {
+    tween.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    tween.reverse();
+  });
+});
+
+const scrambleLinkedin = gsap.utils.toArray('.linkedin');
+scrambleLinkedin.forEach(item => {
+  let tween = gsap.to(item, {
+    duration: 1,
+    scrambleText:{
+      text:"リンクイン",
+      chars:"ウビロミャ"
+    },
+    paused: true
+  });
+  item.addEventListener('mouseenter', () => {
+    tween.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    tween.reverse();
+  });
+});
