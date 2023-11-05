25c4b054593c28d6161d39d6e49d6a85b7c2cca4
Muhammad Irsyad <irsyart@gmail.com>
2023-11-05 22:25:06 +0700

    chore: adjust proportion

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ VANTA.TRUNK({
   scaleMobile: 1.00,
   color: 0xd7d7d7,
   backgroundColor: 0x202020,
-  spacing: smallScreen.matches ? 4.00 : 8.00,
-  chaos: smallScreen.matches ? 4.00 : 7.00
+  spacing: smallScreen.matches ? 2.00 : 8.00,
+  chaos: smallScreen.matches ? 3.00 : 7.00
 })
 
 gsap.registerPlugin(ScrambleTextPlugin);
